1df10aae31a6fa1d6ff7e1894c242bfa196b3226
axmitchell <axmitchell202@gmail.com>
2020-03-14 22:21:59 -0700

    Implement css-grid

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 
 class App extends Component {
@@ -7,10 +8,32 @@ class App extends Component {
             reviews: []
         }
     }
+    componentDidMount() {
+        axios.get('/api/reviews/:roomid/')
+            .then(console.log)
+            .catch(console.log)
+    }
 
     render() {
         return(
-            <h1>Howdy!</h1>
+            <div id='reviews'>
+                <div id='reviewsHeader'>
+                    <h1>Reviews</h1>
+                    <img id='ratingStar' src="https://www.pikpng.com/pngl/m/30-301248_file-gold-star-svg-yellow-star-transparent-background.png" alt="File - Gold Star - Svg - Yellow Star Transparent Background Clipart@pikpng.com"/>
+                    <h3>4.92</h3> 
+                    <h3>273 reviews</h3>
+                    <input placeholder='Search reviews'></input>
+                </div>
+                <div id='reviewsRatings'>
+                    <h3>Some Ratings</h3>
+                </div>
+                <div id='reviewsMain'>
+                    <h3>Some reviews</h3>
+                </div>  
+                <div id='reviewsNavigator'>
+                    <h3>Pagination</h3>
+                </div>
+            </div>
         )
     }
 }
