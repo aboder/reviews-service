c77f230cbada771a92c73331eaa75e562ecd1413
alex <axmitchell202@gmail.com>
2020-03-18 11:07:37 -0700

    include date sort to reviews query

--- client/components/Review.jsx
+++ client/components/Review.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class App extends Component {
+class Review extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -15,4 +15,4 @@ class App extends Component {
     );
   }
 }
-export default App;
+export default Review;
