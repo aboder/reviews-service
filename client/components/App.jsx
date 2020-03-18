b83fcaee6f05742ab5505a61408a5b0b80337500
alex <axmitchell202@gmail.com>
2020-03-18 16:49:35 -0700

    Implement Pagination

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Header from './Header';
 import RatingsList from './RatingsList';
 import ReviewsList from './ReviewsList';
+import Pagination from './Pagination';
 
 class App extends Component {
   constructor(props) {
@@ -11,22 +12,42 @@ class App extends Component {
     this.state = {
       rating: {},
       reviews: [],
+      reviewGroup: 0,
     };
+    this.updateReviewGroup = this.updateReviewGroup.bind(this);
   }
 
   componentDidMount() {
-    axios.get('/api/reviews/0')
+    axios.get('/api/reviews/0/')
       .then((res) => this.setState(res.data))
       .catch(console.log);
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const { reviewGroup } = this.state;
+    if (prevState.reviewGroup !== reviewGroup) {
+      axios.get(`/api/reviews/0/?reviewgroup=${reviewGroup}`)
+        .then((res) => this.setState({
+          reviews: res.data.reviews,
+        }))
+        .catch(console.log);
+    }
+  }
+
+  updateReviewGroup(newReviewGroup) {
+    this.setState({
+      reviewGroup: newReviewGroup,
+    });
+  }
+
   render() {
-    const { rating, reviews } = this.state;
+    const { rating, reviews, reviewGroup } = this.state;
     return (
       <div>
         <Header rating={rating.overall} />
         <RatingsList rating={rating} />
         <ReviewsList reviews={reviews} />
+        <Pagination reviewGroup={reviewGroup} updateReviewGroup={this.updateReviewGroup} />
       </div>
     );
   }
---
+++ client/components/Pagination.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+
+class Pagination extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      previous: false,
+      next: true,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleStateChange = this.handleStateChange.bind(this);
+    this.determineButtonVisiblility = this.determineButtonVisiblility.bind(this);
+  }
+
+  componentDidMount() {
+    document.getElementById('prev').style.visibility = 'hidden';
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { previous, next } = this.state;
+    if (prevState.previous !== previous || prevState.next !== next) {
+      this.determineButtonVisiblility();
+    }
+  }
+
+  determineButtonVisiblility() {
+    const { previous, next } = this.state;
+    if (previous === false) {
+      document.getElementById('prev').style.visibility = 'hidden';
+    } else if (previous === true) {
+      document.getElementById('prev').style.visibility = 'visible';
+    }
+
+    if (next === false) {
+      document.getElementById('next').style.visibility = 'hidden';
+    } else if (next === true) {
+      document.getElementById('next').style.visibility = 'visible';
+    }
+  }
+
+  handleClick(direction) {
+    const { reviewGroup, updateReviewGroup } = this.props;
+    const newReviewGroup = reviewGroup + direction;
+    const newReviewGroupIsWithinBounds = newReviewGroup <= 9 && newReviewGroup >= 0;
+    if (newReviewGroupIsWithinBounds) {
+      updateReviewGroup(newReviewGroup);
+      this.handleStateChange(newReviewGroup);
+    }
+  }
+
+  handleStateChange(newReviewGroup) {
+    const { previous, next } = this.state;
+    if ((newReviewGroup > 0 && newReviewGroup < 9) && (previous === false || next === false)) {
+      this.setState({
+        previous: true,
+        next: true,
+      });
+    } else if (newReviewGroup === 0 && previous === true) {
+      this.setState({
+        previous: false,
+      });
+    } else if (newReviewGroup === 9 && next === true) {
+      this.setState({
+        next: false,
+      });
+    }
+  }
+
+  render() {
+    const previousButton = <button id='prev' onClick={()=>this.handleClick(-1)}>Prev</button>
+    const nextButton = <button id='next' onClick={()=>this.handleClick(1)}>Next</button>
+
+    return (
+      <div>
+        {previousButton}
+        {nextButton}
+      </div>
+    );
+  }
+}
+
+export default Pagination;
