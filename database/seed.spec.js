3da3c4be1e824f11904b938d22157bd51dd762de
alex <axmitchell202@gmail.com>
2020-03-17 20:49:20 -0700

    Include further jest tests for seed script

--- database/seed.spec.js
+++ database/seed.spec.js
@@ -1,5 +1,20 @@
 const seedFunctions = require('./seed.js');
 
-test('it should generate an array containing 5000 objects', () => {
+test('generate5000Reviews should generate an array containing 5000 objects', () => {
   expect(seedFunctions.generate5000Reviews()).toHaveLength(5000);
+});
+
+test('generateAverageRating should generate an object with the correct properties', () => {
+  expect(seedFunctions.generateAverageRating()).toMatchObject({
+    accuracy: expect.any(Number),
+    location: expect.any(Number),
+    cleanliness: expect.any(Number),
+    communication: expect.any(Number),
+    checkIn: expect.any(Number),
+    overall: expect.any(Number),
+  });
+});
+
+test('generate100Rooms should generate an array containing 100 objects', () => {
+  expect(seedFunctions.generate100Rooms()).toHaveLength(100);
 })
