9e7d4990162be37f1e3f2010e3e1f8c259310ea5
alex <axmitchell202@gmail.com>
2020-03-19 18:05:51 -0700

    Implement working infinite scroll hybrid design

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import Header from './Header';
 import RatingsList from './RatingsList';
 import ReviewsList from './ReviewsList';
-import Pagination from './Pagination';
 
 class App extends Component {
   constructor(props) {
@@ -15,6 +14,8 @@ class App extends Component {
       reviewGroup: 0,
     };
     this.updateReviewGroup = this.updateReviewGroup.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.increaseVisibleReviews = this.increaseVisibleReviews.bind(this);
   }
 
   componentDidMount() {
@@ -23,17 +24,26 @@ class App extends Component {
       .catch(console.log);
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    const { reviewGroup } = this.state;
-    if (prevState.reviewGroup !== reviewGroup) {
-      axios.get(`/api/reviews/0/?reviewgroup=${reviewGroup}`)
-        .then((res) => this.setState({
-          reviews: res.data.reviews,
-        }))
-        .catch(console.log);
+  handleScroll(e) {
+    const element = e.target;
+    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+      this.increaseVisibleReviews();
     }
   }
 
+  async increaseVisibleReviews() {
+    try {
+      const { reviewGroup, reviews } = this.state;
+      const updatedReviewGroup = reviewGroup + 1;
+      const additionalReviews = await axios.get(`/api/reviews/0/?reviewgroup=${updatedReviewGroup}`);
+      let updatedReviews = reviews;
+      updatedReviews = updatedReviews.concat(additionalReviews.data.reviews);
+      this.setState({
+        reviews: updatedReviews,
+        reviewGroup: updatedReviewGroup,
+      });
+    } catch (error) { console.log(error); }
+  }
 
   updateReviewGroup(newReviewGroup) {
     this.setState({
@@ -47,8 +57,9 @@ class App extends Component {
       <div id='reviewsComponent'>
         <Header rating={rating.overall} />
         <RatingsList rating={rating} />
-        <ReviewsList reviews={reviews} />
-        <Pagination reviewGroup={reviewGroup} updateReviewGroup={this.updateReviewGroup} />
+        <div id="reviewsScroller" onScroll={this.handleScroll}>
+          <ReviewsList reviews={reviews} />
+        </div>
       </div>
     );
   }
