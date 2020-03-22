3c223944b8bcee909879ae21e3b9a80043964de0
alex <axmitchell202@gmail.com>
2020-03-22 00:01:35 -0400

    continue work

--- client/components/Pagination.jsx
+++ client/components/Pagination.jsx
@@ -26,15 +26,18 @@ class Pagination extends Component {
   render() {
     const { reviewGroup, numOfReviews } = this.props;
     const lastReviewGroup = numOfReviews % 7 === 0 ? (numOfReviews / 7) - 1 : Math.floor(numOfReviews / 7);
-    const previousButtonText = reviewGroup !== 0 ? '<' : 1;
-    const nextButtonText = reviewGroup !== lastReviewGroup ? '>' : lastReviewGroup + 1;
+    const previousButtonText = reviewGroup !== 0 ? '< ' : 1;
+    const nextButtonText = reviewGroup !== lastReviewGroup ? ' >' : lastReviewGroup + 1;
+    const firstButtonText = previousButtonText === 1 ? null : 1;
+    const middleText = reviewGroup !== 0 && reviewGroup !== lastReviewGroup ? ' ... ' + (reviewGroup + 1) + ' ... ' : ' ... ';
+    const lastButtonText = nextButtonText === lastReviewGroup + 1 ? null : lastReviewGroup + 1;
     return (
       <div id="reviewsComponent-pagination">
-        <button id='prev' onClick={()=>this.handleClick(reviewGroup-1)}>{previousButtonText}</button>
-        <button id='firstButton' onClick={()=>this.handleClick(0)}>1</button>
-        {'...'}
-        <button id='lastButton' onClick={()=>this.handleClick(lastReviewGroup)}>{lastReviewGroup + 1}</button>
-        <button id='next' onClick={()=>this.handleClick(reviewGroup+1)}>{nextButtonText}</button>
+        <div id='prev' className='reviewsComponent-pagination-button' onClick={()=>this.handleClick(reviewGroup-1)}>{previousButtonText}</div>
+        <div id='firstButton' className='reviewsComponent-pagination-button' onClick={()=>this.handleClick(0)}>{firstButtonText}</div>
+        <div className='reviewsComponent-pagination-button'>{middleText}</div>
+        <div id='lastButton' className='reviewsComponent-pagination-button' onClick={()=>this.handleClick(lastReviewGroup)}>{lastButtonText}</div>
+        <div id='next' className='reviewsComponent-pagination-button' onClick={()=>this.handleClick(reviewGroup+1)}>{nextButtonText}</div>
       </div>
     );
   }
