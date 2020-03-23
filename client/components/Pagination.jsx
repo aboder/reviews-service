3752942b76b32dffbf3f2f4e28e3694627ca6f81
alex <axmitchell202@gmail.com>
2020-03-23 02:28:59 -0400

    add pagination styles

--- client/components/Pagination.jsx
+++ client/components/Pagination.jsx
@@ -10,14 +10,14 @@ class Pagination extends Component {
 
 
   handleClick(newReviewGroup) {
-    const { updateReviews, numOfReviews, reviewGroup } = this.props;
+    const { updateReviews, numOfReviews } = this.props;
     const lastReviewGroup = numOfReviews % 7 === 0 ? (numOfReviews / 7) - 1 : Math.floor(numOfReviews / 7);
     const newReviewGroupIsWithinBounds = newReviewGroup <= lastReviewGroup && newReviewGroup >= 0;
     if (newReviewGroupIsWithinBounds) {
       updateReviews(newReviewGroup);
-      const topOfReviewDiv = document.getElementById('reviewsComponent-reviews').offsetTop - 10;
+      const topOfReviewsListDiv = document.getElementById('reviewsComponent-reviews').offsetTop - 10;
       window.scrollTo({
-        top: topOfReviewDiv,
+        top: topOfReviewsListDiv,
         behavior: 'smooth',
       });
     }
@@ -28,18 +28,37 @@ class Pagination extends Component {
     const lastReviewGroup = numOfReviews % 7 === 0 ? (numOfReviews / 7) - 1 : Math.floor(numOfReviews / 7);
     const previousButtonText = reviewGroup !== 0 ? '< ' : 1;
     const nextButtonText = reviewGroup !== lastReviewGroup ? ' >' : lastReviewGroup + 1;
-    const firstButtonText = previousButtonText === 1 ? null : 1;
-    const middleText = reviewGroup !== 0 && reviewGroup !== lastReviewGroup ? ' ... ' + (reviewGroup + 1) + ' ... ' : ' ... ';
-    const lastButtonText = nextButtonText === lastReviewGroup + 1 ? null : lastReviewGroup + 1;
-    return (
-      <div id="reviewsComponent-pagination">
-        <div id='prev' className='reviewsComponent-pagination-button' onClick={()=>this.handleClick(reviewGroup-1)}>{previousButtonText}</div>
-        <div id='firstButton' className='reviewsComponent-pagination-button' onClick={()=>this.handleClick(0)}>{firstButtonText}</div>
-        <div className='reviewsComponent-pagination-button'>{middleText}</div>
-        <div id='lastButton' className='reviewsComponent-pagination-button' onClick={()=>this.handleClick(lastReviewGroup)}>{lastButtonText}</div>
-        <div id='next' className='reviewsComponent-pagination-button' onClick={()=>this.handleClick(reviewGroup+1)}>{nextButtonText}</div>
-      </div>
-    );
+    if (reviewGroup === 0) {
+      return (
+        <div id="reviewsComponent-pagination">
+          <div id='prev' className={'pagination-highlighted-element currentPage'} onClick={()=>this.handleClick(reviewGroup-1)}>{previousButtonText}</div>
+          <div className='floats'>...</div>
+          <div id='lastButton' onClick={()=>this.handleClick(lastReviewGroup)}>{lastReviewGroup + 1}</div>
+          <div id='next' className='pagination-highlighted-element'  onClick={()=>this.handleClick(reviewGroup+1)}>{nextButtonText}</div>
+        </div>
+      );
+    } else if (reviewGroup === lastReviewGroup) {
+      return (
+        <div id="reviewsComponent-pagination">
+          <div id='prev' className='pagination-highlighted-element'  onClick={()=>this.handleClick(reviewGroup-1)}>{previousButtonText}</div>
+          <div id='firstButton' onClick={()=>this.handleClick(0)}>1</div>
+          <div className='floats'>...</div>
+          <div id='next' className={'pagination-highlighted-element currentPage'}  onClick={()=>this.handleClick(reviewGroup+1)}>{nextButtonText}</div>
+        </div>
+      )
+    } else {
+      return (
+        <div id="reviewsComponent-pagination">
+          <div id='prev' className='pagination-highlighted-element'  onClick={()=>this.handleClick(reviewGroup-1)}>{previousButtonText}</div>
+          <div id='firstButton' onClick={()=>this.handleClick(0)}>1</div>
+          <div className='floats'>...</div>
+          <div className={'pagination-highlighted-element currentPage'} >{reviewGroup + 1}</div>
+          <div className='floats'>...</div>
+          <div id='lastButton' onClick={()=>this.handleClick(lastReviewGroup)}>{lastReviewGroup + 1}</div>
+          <div id='next' className='pagination-highlighted-element'  onClick={()=>this.handleClick(reviewGroup+1)}>{nextButtonText}</div>
+        </div>
+      );
+    }
   }
 }
 
